2809213cea8266debbc0aa624bb40b87df8da14e
LishunSLD <seanlarrydriesen@gmail.com>
2025-10-03 14:30:37 -0400

    First Depot

---
+++ projet/script/player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+
+@onready var animation: AnimatedSprite2D = $AnimatedSprite2D
+
+const SPEED = 150.0
+
+var directionName = "down";
+
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
+
+	var direction = Vector2(
+		Input.get_axis("left", "right"),
+		Input.get_axis("up", "down")
+	)
+
+	if direction != Vector2.ZERO:
+		velocity = direction.normalized() * SPEED
+	else:
+		velocity = Vector2.ZERO
+
+	if direction != Vector2.ZERO:
+		if abs(direction.x) > abs(direction.y):
+			if direction.x > 0:
+				directionName = "right";
+			else:
+				directionName = "left";
+		else:
+			if direction.y < 0:
+				directionName = "up";
+			else:
+				directionName = "down";
+	
+	if direction == Vector2.ZERO:
+		if directionName == "up":
+			animation.play("idle_up");
+		else:
+			animation.play("idle_down");
+	else:
+		animation.play("walk_" + directionName);
+
+	move_and_slide()
